baa43d3614a16428ece360c7d747118ce55896a6
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-14 20:05:44 +0300

    form js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,11 +2,21 @@ const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
 const formValue = {};
 
+const input = form.elements.email;
+const textarea = form.elements.message;
+if (localStorage.getItem(localStorageKey)!== null) {
+    input.value = (JSON.parse(localStorage.getItem(localStorageKey))).email;
+    textarea.value = (JSON.parse(localStorage.getItem(localStorageKey))).message;
+};
+
 form.addEventListener("input", (ivent) => {
     
     formValue[ivent.target.name] = ivent.target.value
     localStorage.setItem(localStorageKey, JSON.stringify(formValue));
+    
 });
+
+
 
 form.addEventListener("submit", (ivent) => {
     ivent.preventDefault();
