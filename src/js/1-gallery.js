7f947ad189583671268ba67710c9070096805956
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-15 13:36:06 +0300

    SimpleLightbox

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,7 @@
+import { SimpleLightbox } from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 const images = [
   {
     preview:
@@ -77,10 +81,10 @@ const createMarkup = images.map(({ preview, original, description }) => {
         
 galleryContainer.insertAdjacentHTML("beforeend", createMarkup);
 
-import { SimpleLightbox } from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-var lightbox = new SimpleLightbox('.gallery a', {});
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: .8,
+  captionsData: "alt",
+  captionsDelav: 250
+});
 
 
