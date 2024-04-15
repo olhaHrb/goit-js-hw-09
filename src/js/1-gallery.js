836e0dc8a42b5ca059bb667834b7ae3a2114e938
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-15 17:51:16 +0300

    SimpleLightbox new

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,4 @@
-import { SimpleLightbox } from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
@@ -83,7 +83,7 @@ galleryContainer.insertAdjacentHTML("beforeend", createMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: "alt",
-  captionsDelav: 250
+  captionsDelay: 250
 });
 
 
